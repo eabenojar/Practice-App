a5a7d5494ac1be1b27b1430a96c384b22af7e396
Eric Abenojar <abenoeric@gmail.com>
2017-01-30 13:50:12 -0800

    update

--- app.js
+++ app.js
@@ -1,33 +1,54 @@
-var fs = require('fs');
-var prod = process.argv.indexOf('--product');
-var sum = 0;
-if(prod >= 0){
-	for(var i = 0; i<prod.length; i++){
-		sum += parseInt(prod[i]);
+var args = process.argv;
+var flagsPresent = [];
+
+args.forEach(function(item){
+	if(item === "---sum" || item === "---product"){
+		flagsPresent.push();
 	}
+});
+
+if(flagsPresent.length !== 1){
+	throw 'error: Incorrect number of flags present';
 }
 
-console.log(sum);
+var isNumber = function(x){
+	return !isNaN(parseFloat(x));
+}
 
-if(process.argv.length <= 3){
-	console.log('error: not enough arguments');
-	return null;
+var numbers = args.filter(isNumber).map(parseFloat);
+var result;
+
+if(flagsPresent[0] === '--sum'){
+	result = numbers.reduce(function(a,b){ return a + b; });
 }
-if(process.argv[2] === '--sum' || process.argv[2] === '--product'){
-	if(process.argv[2] === '--sum'){
-		var sum=0;
-		for(var i=3; i<process.argv.length; i++){
-			sum += parseFloat(process.argv[i]);
-		}
-		console.log(sum);
-	}else{
-		var prod = 1;
-		for(var i=3; i<process.argv.length; i++){
-			prod *= parseFloat(process.argv[i]);
-		}
-		console.log(prod);
-	}
-} else{
-	console.log('error: no flag was given');
-	return null;
+
+if(flagsPresent[0] === '--product'){
+	results = numbers.reduce(function(a,b){return a * b;})
 }
+console.log("hey");
+console.log("result is %d", result);
+
+
+
+// if(process.argv.length <= 3){
+// 	console.log('error: not enough arguments');
+// 	return null;
+// }
+// if(process.argv[2] === '--sum' || process.argv[2] === '--product'){
+// 	if(process.argv[2] === '--sum'){
+// 		var sum=0;
+// 		for(var i=3; i<process.argv.length; i++){
+// 			sum += parseFloat(process.argv[i]);
+// 		}
+// 		console.log(sum);
+// 	}else{
+// 		var prod = 1;
+// 		for(var i=3; i<process.argv.length; i++){
+// 			prod *= parseFloat(process.argv[i]);
+// 		}
+// 		console.log(prod);
+// 	}
+// } else{
+// 	console.log('error: no flag was given');
+// 	return null;
+// }
